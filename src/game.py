b4bc8440925054517bc2be3ecc2ba1a79a3695a9
MartaMariz <61186044+MartaMariz@users.noreply.github.com>
2022-04-12 09:44:23 +0100

    structure chagend

--- src/game.py
+++ src/game.py
@@ -1,30 +1,38 @@
+from re import S
 from piece import Bishop, Piece, Tower, Queen, Horse
 from snake import Snake
 
-class GameState:
+class Game:
+        __pieces = []
         __board = [[]]
-        __snake_c = 0
-        __snake_l = 0
+        __snake = 0
 
-        def __init__(self):
-                __board = [
-                        [None, None, None, None, None],
-                        [None, None, None, None, None],
-                        [None, None, Queen(2, 2, 5, 5), None, None],
-                        [1,   1, None, None, None],
-                        [Snake, None, None, None, None]
-                        ]
-                
+        def __init__(lines, cols, positions, self):
+
+                self.init_board( lines, cols, positions)
+
+                self.init_snake( lines, cols)
+
+                for piece in positions:
+                        if ( piece[0] == 'H'):
+                                self.__pieces.append( Tower(lines, cols, positions))
+
         
         def up(self): 
                 if ( self.__snake_pos[0] > 0 ): return 0
 
-                if (self.__snake_l + 1 < len( self.__board ):
-                if (self.__board[self.__snake_c][self.__snake_l] )
+                if (self.__snake_l - 1 > 0 ): 
+                        return 0
+        
+        def printBoard(self):
+                if (self.__board[2][2] is not None):
+                        self.__board[2][2].printAttack()
+
+
+board = Game( 5, 5, ['H', 1, 3])
+board.printBoard()
 
 
 
 
 
-if (board[2][2] is not None):
-    board[2][2].printAttack()
